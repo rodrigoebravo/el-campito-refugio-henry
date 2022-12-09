47bc8dfb898f8af594649dd4f7a4f73e643a84de
reuth <jhosephvillalba@gmail.com>
2022-12-09 07:46:05 -0500

    modulo de arranque y morgan

--- api/app.js
+++ api/app.js
@@ -1,22 +1,17 @@
 const express = require("express"); 
 const cors = require("cors"); 
-require("dotenv").config(); 
-const dbConnect = require("./config/mongo"); 
+const morgan = require("morgan"); 
 const app = express(); 
 
 
-//midleware
+app.use(morgan('tiny'))
 app.use(cors()); //error de origen cruzado
 app.use(express.json()); //Manejar data .json
 
 
-//router 
+
 app.use("/api", require("./routers")); 
 
-app.listen(process.env.PORT, ()=>{
-	console.log(`http://localhost:${process.env.PORT}`); 
-  
-}); 
 
+module.exports = app; 
 
-dbConnect();
--- api/config/mongo.js
+++ api/config/mongo.js
@@ -3,23 +3,13 @@ const mongoose = require("mongoose");
 const dbConnect = () => {
   const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wiasisw.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`;
 
-  mongoose.connect(
+  return mongoose.connect(
     DB_URI,
     {
-      //opctions
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
-    (err, res) => {
-      
-      if (!err) {
-        console.log("***established connection***");
-      }else{
 
-        console.log("***connection error***");
-      }
-      
-    }
   );
 };
 
