6300e6aef8c6effd50e5fd9e36cbf0a5e0768155
Enzo Calvi <107884279+E1Eternauta@users.noreply.github.com>
2022-12-11 15:21:20 -0300

    Nuevas rutas admin en el back

--- api/routers/dogs.js
+++ api/routers/dogs.js
@@ -1,12 +1,27 @@
-const router = require("express").Router(); 
-const { getDogs, getDogById, createDog, updateDog } = require("../controllers/dogs"); 
+const router = require("express").Router();
+const {
+  getDogs,
+  getDogById,
+  createDog,
+  updateDog,
+  adminDogs,
+  adminDogsID,
+  adminUpdate,
+  adminDelete,
+} = require("../controllers/dogs");
 
-router.get("/", getDogs); 
-router.get("/:id", getDogById)
+router.get("/", getDogs);
+router.get("/:id", getDogById);
 router.post("/", createDog);
 
-router.put("/", updateDog); 
+router.get("/admin/dogs", adminDogs);
+router.get("/admin/dogs/:id", adminDogsID);
 
-// router.delete("/:id", deleteDog); 
+router.put("/", updateDog);
+router.put("/admin/dogs/:id", adminUpdate);
 
-module.exports = router; 
+router.delete("/admin/dogs/:id", adminDelete);
+
+// router.delete("/:id", deleteDog);
+
+module.exports = router;
