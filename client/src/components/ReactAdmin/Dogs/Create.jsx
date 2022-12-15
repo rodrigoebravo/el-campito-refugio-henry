cf196eab06bfdf4b27f7ee916fb274fb332b97b2
Matias <matiasdentoni@gmail.com>
2022-12-15 08:34:49 -0300

    Dashoard+modelsDB

--- client/src/components/ReactAdmin/Dogs/Create.jsx
+++ client/src/components/ReactAdmin/Dogs/Create.jsx
@@ -1,4 +1,4 @@
-import { Create, SimpleForm, TextInput, ImageInput,
+import { Create, SimpleForm, TextInput, ImageInput, SelectInput, BooleanInput,
   CheckboxGroupInput, ImageField } from "react-admin";
 
 const PostCreate = (props) => {
@@ -7,23 +7,37 @@ const PostCreate = (props) => {
       <SimpleForm>
         <TextInput source="id" disabled fullWidth />
         <TextInput source="name" label="Nombre" fullWidth />
-        <TextInput source="gender" label="Sexo" fullWidth />
-        <TextInput source="size" label="Tamaño" fullWidth />
+        <SelectInput source="gender" label="Sexo" choices={[
+            { id: 'hembra', name: 'hembra' },
+            { id: 'macho', name: 'macho' },
+        ]} />
+        <SelectInput source="size" label="Tamaño" choices={[
+            { id: 'chico', name: 'chico' },
+            { id: 'mediano', name: 'mediano' },
+            { id: 'grande', name: 'grande' },
+        ]} />
+        <SelectInput source="age" label="Edad" choices={[
+            { id: 'adulto', name: 'adulto' },
+            { id: 'adulto jóven', name: 'adulto jóven' },
+            { id: 'viejito', name: 'viejito' },
+            { id: 'cachorro', name: 'cachorro' },
+        ]} />
         <TextInput source="race" label="Raza" fullWidth />
-        <CheckboxGroupInput source="roles" choices={[
-            { id: 'a000', name: 'Se lleva con perros' },
-            { id: 'u001', name: 'Se lleva con chicos	' },
+        <CheckboxGroupInput source="references" label="Referencias" choices={[
+            { id: 'u000', name: 'Se lleva con perros' },
+            { id: 'u001', name: 'Se lleva con hembras	' },
             { id: 'u002', name: 'Cuidados especiales	' },
             { id: 'u003', name: 'Discapacitado' },
-            { id: 'a004', name: 'Carácter especial' },
+            { id: 'u004', name: 'Carácter especial' },
         ]} />
-        <TextInput source="references" label="Caracteristicas" fullWidth />
+        <TextInput source="features" label="Caracteristicas" fullWidth />
         <ImageInput source="images" label="Fotografías" multiple>
             <ImageField source="src" title="title" />
         </ImageInput>
         <ImageInput source="video" label="Video" >
             <ImageField source="src" title="title" />
         </ImageInput>
+        <BooleanInput label="¿ Se puede apadrinar ?" source="isSponsored" />
       </SimpleForm>
     </Create>
   );
