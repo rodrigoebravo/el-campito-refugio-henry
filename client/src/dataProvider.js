4449c203fdc87f09f5c2468d0b55d6e248843721
reuth <jhosephvillalba@gmail.com>
2022-12-31 11:26:52 -0500

    mejoras dataProvider

--- client/src/dataProvider.js
+++ client/src/dataProvider.js
@@ -65,41 +65,68 @@ const dataProvider = {
   },
 
   update: async (resource, params) => {
-    if (
-      resource === "api/admin/users" &&
-      params.data.image.hasOwnProperty("rawFile")
-    ) {
-      console.log(params.data.image);
-      params.data.image = await pushCloudinary(params.data.image);
-    }
 
-    if (
-      resource === "api/admin/dogs" &&
-      params.data.images.some((e) => e.hasOwnProperty("rawFile"))
-    ) {
-      let newImages = params.data.images.filter(
-        (img) => typeof img !== "string"
-      );
-      params.data.images = newImages;
-      params.data.images = await pushCloudinary(newImages);
-    }
+    switch (resource) {
+      case "api/admin/users":
+        if(  params.data.image === null) params.data.image = ""; 
+        else if( params.data.image.hasOwnProperty("rawFile")) 
+        params.data.image = await pushCloudinary(params.data.image); 
+        else params.data.image = params.data.image.src; 
+        break;
 
-    if (
-      resource === "api/admin/press" &&
-      params.data.img.hasOwnProperty("rawFile")
-    ) {
-      params.data.img = await pushCloudinary(params.data.img);
-    }
+      case "api/admin/dogs":
+        if (params.data.images.every((img) => img.hasOwnProperty("rawFile")))
+          params.data.images = await pushCloudinary(params.data.images);
+        else if (params.data.images.every((img) => img.hasOwnProperty("index")))
+          params.data.images = params.data.images.map((img) => img.src);
+        else {
+          const oldImages = params.data.images.filter((img) =>
+            img.hasOwnProperty("src")
+          );
+          const urlsOld = oldImages.map((img) => img.src);
+          const newImg = params.data.images.filter((img) => img.rawFile);
 
-    if (
-      resource === "api/admin/interfaces" 
-    ) {
-      params.data.slider = await pushCloudinary(params.data.slider);
-      params.data.imgNosotros = await pushCloudinary(params.data.imgNosotros);
-      // params.data.imgNosotros = await pushCloudinary(params.data.imgColabora); // NO ESTA EN LOS CAMPOS DE INTERFACES!!!!
-      params.data.imgVoluntarios = await pushCloudinary(
-        params.data.imgVoluntarios
-      );
+          const urlNew = await pushCloudinary(newImg);
+          params.data.images = [...urlsOld, ...urlNew];
+        }
+        break;
+      case "api/admin/interfaces":
+        if(params.data.imgVoluntarios === null) params.data.imgVoluntarios = ""; 
+        else if (params.data.imgVoluntarios.hasOwnProperty("rawFile"))
+          params.data.imgVoluntarios = await pushCloudinary(
+            params.data.imgVoluntarios
+          );
+        else params.data.imgVoluntarios = params.data.imgVoluntarios.src;
+
+        if(params.data.imgNosotros === null) params.data.imgNosotros = ""; 
+        else if (params.data.imgNosotros.hasOwnProperty("rawFile"))
+          params.data.imgNosotros = await pushCloudinary(
+            params.data.imgNosotros
+          );
+        else params.data.imgVoluntarios = params.data.imgVoluntarios.src;
+
+        if (params.data.slider.length > 0 ) {
+          let newSlider = []
+          params.data.slider.forEach( async(e) => {
+            if (e.hasOwnProperty("rawFile")) {
+              e = await pushCloudinary( e );
+            } else { 
+              e = e.src
+            };
+            newSlider.push(e)
+          });
+          params.data.slider = newSlider;
+        }
+  
+        break;
+      case "api/admin/press":
+        if(params.data.img === null) params.data.img = ""; 
+        else if (params.data.img.hasOwnProperty("rawFile"))
+          params.data.img = await pushCloudinary(params.data.img);
+        else params.data.img = params.data.img.src;
+        break;
+      default:
+        break;
     }
 
     const { json } = await httpClient(`${apiUrl}/${resource}/${params.id}`, {
