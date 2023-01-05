8d0941708515e0ff3641d3a8f599fa6a788bbeec
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-01-05 11:47:35 -0300

    solucion de conflictos 3

--- api/middlewares/mailer.js
+++ api/middlewares/mailer.js
@@ -6,44 +6,52 @@ const nodemailer = require("nodemailer");
 const mailer = async (req, res, next) => {
   try {
     const { email, pass } = req.body;
+    let url = req.url;
+    let route = url.split("/").pop(); 
 
-    // let user = await usersModel.findOne({ email });
-
-    // if (user) {
-    //   // res.status(200).send("usuario registrado");
-    //   console.log("usuario registrado");
-    // } else {
-      let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: process.env.GMAIL_USER, // generated ethereal user
-          pass: process.env.GMAIL_PASSWORD, // generated ethereal password
-        },
-      });
-
-      // send mail with defined transport object
-
-      await transporter.sendMail({
-        from: '"El campito Refugio" <elcampitorefugiodev@gmail.com>', // sender address
-        to: `${email}`, // list of receivers
-        subject: `Bienvenido ${pass} `, // Subject line
-        html: `
-            <h2>Se inicio session con el email : ${email} en el Campito Regufio!</h2>
-            <br>
-                  <p>Ingresa <a href=${"http://localhost:3000"} style="color:blue">AQUI</a></p>
-                    <br>
+    const messages = {
+      login: `
+      <h2>Se inicio session con el email : ${email} en el Campito Regufio!</h2>
+      <br>
+            <p>Ingresa <a href=${"http://localhost:3000"} style="color:blue">AQUI</a></p>
+              <br>
+              <img src='https://doprod-statics.s3.amazonaws.com/pictures/logo/9343/large_fit_6709cfc0-d706-49ed-ba8e-d9682a4b8305.png' alt='Elcampito.jpeg' />
+              <br>
+              <p>El campito refugio staff.</p>`,
+    
+      register: `<h2>Registro>registro exitoso en el Campito Regufio!</h2>
+                <p>Correo: ${email}</p>
+                <p>Contraseña: ${pass}</p>
+                <br>
                     <img src='https://doprod-statics.s3.amazonaws.com/pictures/logo/9343/large_fit_6709cfc0-d706-49ed-ba8e-d9682a4b8305.png' alt='Elcampito.jpeg' />
-                    <br>
-                    <p>El campito refugio staff.</p>`, // html body
-      });
-    // }
-    next();
+                <br>`,
+    };
+  
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.GMAIL_USER, // generated ethereal user
+        pass: process.env.GMAIL_PASSWORD, // generated ethereal password
+      },
+    });
+  
+    // send mail with defined transport object
+  
+    await transporter.sendMail({
+      from: '"El campito Refugio" <elcampitorefugiodev@gmail.com>', // sender address
+      to: `${email}`, // list of receivers
+      subject: `Bienvenido ${pass}`, // Subject line
+      html: messages[route], // html body
+    });
+    
+
+    next(); 
+
   } catch (error) {
     console.error(error);
   }
-};
 };
 
 
--- api/routers/auth.js
+++ api/routers/auth.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const  mailer = require("../middlewares/mailer");
-const  mailerRegister = require("../middlewares/mailerRegister");
 const { login, register } = require("../controllers/session");
 
 
-// router.use(mailer); 
-router.post("/login",mailer, login);
-router.post("/register",mailerRegister, register);
+router.use(mailer); 
+router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
